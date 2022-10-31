1e31c136eef5ac8391c3c744e9cc6d0ba910743e
Jakob Westhoff <jakob@westhoffswelt.de>
2022-10-31 18:04:27 +0100

    Initial bootstrapping of yew + tailwindcss app
    This is the most basic bootstrapping that seems to be out there, for
    a combination of yew and tailwindcss. Currently missing is integration
    of the different build processes.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.rs",
+    "./index.html",
+    "./src/**/*.html",
+    "./src/**/*.css",
+    "./static/**/*.css",
+    "./static/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
