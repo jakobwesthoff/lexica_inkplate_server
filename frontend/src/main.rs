ff5844dedddee0fc1784cb731010b617b23b825f
Jakob Westhoff <jakob@westhoffswelt.de>
2022-11-01 12:57:52 +0100

    Some playing around with tailwind

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -2,8 +2,12 @@ use yew::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
+    let quote_of_the_day = "Much yew, much html, so much much";
     html! {
-        <h1 class={"text-4xl font-bold underline"}>{ "Hello Youtube!" }</h1>
+        <>
+            <h1 class="shadow-xl text-4xl font-bold underline">{ "Hello YouTube" }</h1>
+            <h2 class="text-orange-600 text-3xl">{ quote_of_the_day }</h2>
+        </>
     }
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.rs",
     "./index.html",
     "./src/**/*.html",
-    "./static/**/*.html",
+    "./src/**/*.css",
   ],
   theme: {
     extend: {},
